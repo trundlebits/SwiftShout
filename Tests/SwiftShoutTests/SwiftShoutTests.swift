bbf6c46e50d3542b27cb3150de517e42429a7f74
Brian Manning <211186584+trundlebits@users.noreply.github.com>
2026-04-15 21:51:54 -0700

    SwiftShoutTests.swift: renamed example test

--- Tests/SwiftShoutTests/SwiftShoutTests.swift
+++ Tests/SwiftShoutTests/SwiftShoutTests.swift
@@ -1,7 +1,7 @@
 import Testing
 @testable import SwiftShout
 
-@Test func example() async throws {
+@Test func InitSwiftShout() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     // Swift Testing Documentation
     // https://swiftpackageindex.com/swiftlang/swift-testing/documentation
