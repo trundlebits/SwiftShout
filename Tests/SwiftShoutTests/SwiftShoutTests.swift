cae12270dee2b7696390b13c13045793cd2870ff
Brian Manning <211186584+trundlebits@users.noreply.github.com>
2026-04-15 22:12:37 -0700

    SwiftShoutTests.swift: init a new _SwiftShout()_ object for testing

--- Tests/SwiftShoutTests/SwiftShoutTests.swift
+++ Tests/SwiftShoutTests/SwiftShoutTests.swift
@@ -2,6 +2,7 @@ import Testing
 @testable import SwiftShout
 
 @Test func InitSwiftShout() async throws {
+    _ = SwiftShout()
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     // Swift Testing Documentation
     // https://swiftpackageindex.com/swiftlang/swift-testing/documentation
