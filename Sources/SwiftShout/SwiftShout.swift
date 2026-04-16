cd0156ed9d762532404045869b71c753bdc23c37
Brian Manning <211186584+trundlebits@users.noreply.github.com>
2026-04-15 22:13:38 -0700

    SwiftShout: create pointers for calling `shout_version()`
    - Create a set of `UnsafeMutablePointer<Int32>` pointers to pass to
      `shout_version()` to receive the version numbers from the library

--- Sources/SwiftShout/SwiftShout.swift
+++ Sources/SwiftShout/SwiftShout.swift
@@ -6,6 +6,11 @@ import CShout
 class SwiftShout {
     init() {
         shout_init()
-        let shoutVersion = shout_version(UnsafeMutablePointer<Int32>!, UnsafeMutablePointer<Int32>!, UnsafeMutablePointer<Int32>!)
+        // https://medium.com/@maxches/advanced-memory-management-with-unsafe-swift-f34d5bfbd78f
+        let major = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        let minor = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        let patch = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
+        let shoutVersion = shout_version(major, minor, patch)
+        print("Shout version: \(shoutVersion!)")
     }
 }
