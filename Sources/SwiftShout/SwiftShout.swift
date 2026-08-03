6ea54af7a1fe73e4f8a9eb645b91013d125c4031
Brian Manning <211186584+trundlebits@users.noreply.github.com>
2026-08-02 23:00:42 -0700

    SwiftShout.swift: use Span instead of UnsafeMutablePointer for shout_version
    Replace the manually-allocated, never-deallocated UnsafeMutablePointer
    out-params with a scoped withUnsafeTemporaryAllocation buffer, reading
    the results back through .span instead of .pointee.

--- Sources/SwiftShout/SwiftShout.swift
+++ Sources/SwiftShout/SwiftShout.swift
@@ -6,17 +6,17 @@ import CShout
 class SwiftShout {
     init() {
         shout_init()
-        // https://medium.com/@maxches/advanced-memory-management-with-unsafe-swift-f34d5bfbd78f
-        let major = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
-        let minor = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
-        let patch = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
-        let _ = shout_version(major, minor, patch)
-        // let shoutVersion = shout_version(major, minor, patch)
-        // print("Shout version: \(shoutVersion!)")
 
-        // https://www.kodeco.com/7181017-unsafe-swift-using-pointers-and-interacting-with-c/page/4
-        print("Major: \(major.pointee)")
-        print("Minor: \(minor.pointee)")
-        print("Patch: \(patch.pointee)")
+        withUnsafeTemporaryAllocation(of: Int32.self, capacity: 3) { buffer in
+            let base = buffer.baseAddress!
+            // https://medium.com/@maxches/advanced-memory-management-with-unsafe-swift-f34d5bfbd78f
+            let _ = shout_version(base, base + 1, base + 2)
+
+            // https://www.kodeco.com/7181017-unsafe-swift-using-pointers-and-interacting-with-c/page/4
+            let versions = buffer.span
+            print("Major: \(versions[0])")
+            print("Minor: \(versions[1])")
+            print("Patch: \(versions[2])")
+        }
     }
 }
