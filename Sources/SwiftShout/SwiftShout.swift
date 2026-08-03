075ff03c29b9941a56735dd9e0be8ed19fa55cae
Brian Manning <211186584+trundlebits@users.noreply.github.com>
2026-08-02 23:26:37 -0700

    SwiftShout.swift: drop dead reference comments

--- Sources/SwiftShout/SwiftShout.swift
+++ Sources/SwiftShout/SwiftShout.swift
@@ -9,10 +9,8 @@ class SwiftShout {
 
         withUnsafeTemporaryAllocation(of: Int32.self, capacity: 3) { buffer in
             let base = buffer.baseAddress!
-            // https://medium.com/@maxches/advanced-memory-management-with-unsafe-swift-f34d5bfbd78f
             let _ = shout_version(base, base + 1, base + 2)
 
-            // https://www.kodeco.com/7181017-unsafe-swift-using-pointers-and-interacting-with-c/page/4
             let versions = buffer.span
             print("Major: \(versions[0])")
             print("Minor: \(versions[1])")
